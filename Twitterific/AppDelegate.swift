97be9aade83de20a7245ff9c5a977850ca114582
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-15 01:00:34 -0600

    Twiiter client ready

--- Twitterific/AppDelegate.swift
+++ Twitterific/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        TwitterClient.sharedInstance.fetchAccessTokenWithPath("oauth/access_token", method: "POST", requestToken: BDBOAuth1Credential(queryString: url.query), success: { (accessToken: BDBOAuth1Credential!) in
+            
+            print("Got the access token")
+            TwitterClient.sharedInstance.requestSerializer.saveAccessToken(accessToken)
+            
+            TwitterClient.sharedInstance.GET("1.1/account/verify_credentials.json", parameters: nil, success: { (operation: NSURLSessionDataTask, response: AnyObject?) in
+                
+                }, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+
+            })
+
+        }) { (error: NSError!) in
+                print("Failed to recieve access token")
+        }
+        return true
     }
 
 
--- Twitterific/ViewController.swift
+++ Twitterific/ViewController.swift
@@ -25,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onLogin(sender: AnyObject) {
+        
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "twitterificaudi://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
+            print("Got the request token")
+            
+            let authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            UIApplication.sharedApplication().openURL(authURL!)
+        }) { (error: NSError!) in
+            print("Failed to get request token")
+        }
+    }
 
 }
 
