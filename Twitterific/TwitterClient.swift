9a64a0eed572afec22384b3c6183ee76fefb7894
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-15 00:16:25 -0600

    Create shared instance

--- Twitterific/TwitterClient.swift
+++ Twitterific/TwitterClient.swift
@@ -2,12 +2,23 @@
 //  TwitterClient.swift
 //  Twitterific
 //
-//  Created by Aditya Purandare on 14/02/16.
+//  Created by Aditya Purandare on 15/02/16.
 //  Copyright © 2016 Aditya Purandare. All rights reserved.
 //
 
 import UIKit
+import BDBOAuth1Manager 
+
+let twitterBaseURL = NSURL(string: "https://api.twitter.com")
+let twitterConsumerKey = "3PIBp35bTWx29T72BiKhE2hQD"
+let twitterConsumerSecret = "jACfwaxOB5KH8vfni3xSxYUUGeCRyWDRv57IvsjJfdSE6qGL0C"
 
 class TwitterClient: BDBOAuth1SessionManager {
 
+class var sharedInstance: TwitterClient {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+        }
+    return Static.instance
+    }
 }
--- Twitterific/ViewController.swift
+++ Twitterific/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import BDBOAuth1Manager
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
