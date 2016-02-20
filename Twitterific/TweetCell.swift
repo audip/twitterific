277efcc60c6f00621f9fed8311a5b98c60481b96
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-20 00:28:18 -0600

    Elements laid out

---
+++ Twitterific/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Twitterific
+//
+//  Created by Aditya Purandare on 19/02/16.
+//  Copyright © 2016 Aditya Purandare. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
